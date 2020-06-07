adb2d7ef2039e3ec0f64cfeb347efed48140e370
Arilson Santos <arilson@msn.com>
2020-06-07 01:08:54 -0300

    Encodig passsword

---
+++ src/main/java/br/com/orion/authorizationserver/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package br.com.orion.authorizationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import br.com.orion.authorizationserver.service.UserDetailServiceImpl;
+
+import lombok.AllArgsConstructor;
+
+@EnableWebSecurity
+@EnableResourceServer
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailServiceImpl userDetailServiceImpl;
+    //private PasswordEncoder passwordEncoder;
+
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailServiceImpl).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    //Generate password
+    // public static void main(String[] args) {
+    //     var crypt = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    //     System.out.println(crypt.encode("secret"));
+    // }
+
+}
